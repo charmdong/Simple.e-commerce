9b32b9cc6df3427eb9d06c105869300d5d026b84
Donggun.Chung <chungdk1117@gmail.com>
2022-05-27 15:59:06 +0900

    :hammer: 2022.05.27.Fri [findOrderByRegId 추가]

--- src/main/java/com/commerce/repository/OrderRepository.java
+++ src/main/java/com/commerce/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import com.commerce.domain.Order;
 import com.commerce.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,6 +23,12 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             " join fetch o.orderItems oi" +
             " join fetch oi.item i")
     List<Order> findAllWithItem();
+
+    @Query("select o from Order o" +
+            " inner join o.orderItems ois" +
+            " inner join ois.item i" +
+            " where i.regId = :regId")
+    List<Order> findByRegId(@Param("regId") String regId);
 
     List<Order> findByMemberId(String userId);
 
