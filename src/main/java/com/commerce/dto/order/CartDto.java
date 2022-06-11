798d1a042a2ec95b244a6ff812dc3d8c43246fe6
Donggun.Chung <chungdk1117@gmail.com>
2022-06-11 14:44:32 +0900

    :hammer: 2022.06.11.Sat [장바구니 목록 조회]

--- src/main/java/com/commerce/dto/order/CartDto.java
+++ src/main/java/com/commerce/dto/order/CartDto.java
@@ -10,14 +10,14 @@ import lombok.ToString;
 @ToString
 public class CartDto {
 
-    private Long cartId;
+    private Long id;
     private Long itemId;
     private String itemName;
     private int price;
     private int count;
 
     public CartDto(Cart cart) {
-        this.cartId = cart.getId();
+        this.id = cart.getId();
         this.itemId = cart.getItem().getId();
         this.itemName = cart.getItem().getName();
         this.price = cart.getItem().getPrice();
