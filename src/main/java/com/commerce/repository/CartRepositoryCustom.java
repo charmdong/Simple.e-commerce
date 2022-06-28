08deaeed4395685610db84e03a1316e5f4863d9f
Donggun.Chung <chungdk1117@gmail.com>
2022-06-29 00:52:17 +0900

    :hammer: 2022.06.29.Wed [CartRepositoryCustom 추가]

---
+++ src/main/java/com/commerce/repository/CartRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.commerce.repository;
+
+import com.commerce.domain.Cart;
+
+import java.util.List;
+
+public interface CartRepositoryCustom {
+
+    List<Cart> findAllByMemberId(String memberId);
+}
---
+++ src/main/java/com/commerce/repository/CartRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.commerce.repository;
+
+import com.commerce.domain.Cart;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.commerce.domain.QCart.cart;
+import static com.commerce.domain.QItem.item;
+import static com.commerce.domain.QMember.member;
+
+@RequiredArgsConstructor
+public class CartRepositoryImpl implements CartRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Cart> findAllByMemberId (String memberId) {
+        return queryFactory.selectFrom(cart)
+                .innerJoin(cart.item, item).fetchJoin()
+                .innerJoin(cart.member, member)
+                .where(member.id.eq(memberId))
+                .fetch();
+    }
+}
