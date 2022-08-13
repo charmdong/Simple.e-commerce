c4afdacdfb3fe2a4b0328e7dad574d7f903a99d6
Donggun.Chung <chungdk1117@gmail.com>
2022-08-13 21:48:31 +0900

    :hammer: 2022.08.13.Sat [메시지 추가]

--- src/main/java/com/commerce/message/MessageConstants.java
+++ src/main/java/com/commerce/message/MessageConstants.java
@@ -8,4 +8,7 @@ public class MessageConstants {
 
     public static final String CART_DUP = "이미 장바구니에 존재합니다.";
     public static final String CART_ADD = "장바구니에 담았습니다.";
+    public static final String CART_DELETE = "장바구니에서 삭제되었습니다.";
+    public static final String CARD_UPDATE = "장바구니가 수정되었습니다.";
+
 }
