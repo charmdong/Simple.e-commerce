5633a5034376b3a7cfbed2ce75b9dcb5f7426072
Donggun.Chung <chungdk1117@gmail.com>
2022-08-24 20:15:09 +0900

    :hammer: 2022.08.24.Wed [메시지 상수화]

--- src/main/java/com/commerce/message/MessageConstants.java
+++ src/main/java/com/commerce/message/MessageConstants.java
@@ -9,6 +9,8 @@ public class MessageConstants {
     public static final String CART_DUP = "이미 장바구니에 존재합니다.";
     public static final String CART_ADD = "장바구니에 담았습니다.";
     public static final String CART_DELETE = "장바구니에서 삭제되었습니다.";
-    public static final String CARD_UPDATE = "장바구니가 수정되었습니다.";
+    public static final String CART_UPDATE = "장바구니가 수정되었습니다.";
 
+    public static final String VALID_ID = "사용 가능한 아이디입니다.";
+    public static final String INVALID_ID = "사용할 수 없는 아이디입니다.";
 }
