8226130e20a35a3124ac268107a403c1aaf10243
Donggun.Chung <chungdk1117@gmail.com>
2022-09-10 23:04:45 +0900

    :hammer: 2022.09.10.Sat [주석 수정]

--- src/main/java/com/commerce/controller/CartController.java
+++ src/main/java/com/commerce/controller/CartController.java
@@ -1,13 +1,15 @@
 package com.commerce.controller;
 
-import com.commerce.vo.SessionVO;
-import com.commerce.vo.order.CartVO;
 import com.commerce.service.CartService;
 import com.commerce.util.SessionUtils;
+import com.commerce.vo.SessionVO;
+import com.commerce.vo.order.CartVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
@@ -25,9 +27,18 @@ public class CartController {
      * @return
      */
     @GetMapping("/carts")
-    public String cartList(@SessionAttribute(name = SessionUtils.LOGIN_SESSION) SessionVO session, Model model) {
+    public String cartList(@SessionAttribute(name = SessionUtils.LOGIN_SESSION) SessionVO session,
+                           @RequestParam(name = "itemName", required = false) String itemName, Model model) {
+
         String userId = session.getId();
-        List<CartVO> cartList = cartService.findCartByUserId(userId);
+        List<CartVO> cartList;
+
+        // 상품명 존재
+        if (StringUtils.hasText(itemName)) {
+            cartList = cartService.findCartListByItemName(userId, itemName);
+        } else {
+            cartList = cartService.findCartByUserId(userId);
+        }
 
         model.addAttribute("cartList", cartList);
         return "cart/cartList";
