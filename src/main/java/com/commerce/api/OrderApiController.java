46df531dcc52fa6bf417ae6dcf41d3aeb61e926d
Donggun.Chung <chungdk1117@gmail.com>
2022-09-13 22:40:55 +0900

    :hammer: 2022.09.13.Tue [주문 취소]

--- src/main/java/com/commerce/api/OrderApiController.java
+++ src/main/java/com/commerce/api/OrderApiController.java
@@ -1,19 +1,15 @@
 package com.commerce.api;
 
 import com.commerce.message.MessageConstants;
-import com.commerce.vo.SessionVO;
-import com.commerce.vo.order.OrderItemRequest;
 import com.commerce.service.OrderService;
 import com.commerce.util.SessionUtils;
+import com.commerce.vo.SessionVO;
+import com.commerce.vo.order.OrderItemRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,10 +26,16 @@ public class OrderApiController {
 
     private final OrderService orderService;
 
+    /**
+     * 주문하기
+     * @param sessionVO
+     * @param request
+     * @return
+     * @throws RuntimeException
+     */
     @PostMapping("/order")
-    public ResponseEntity<Map<String, Object>> orderItem (HttpSession session, @RequestBody OrderItemRequest request) throws RuntimeException {
+    public ResponseEntity<Map<String, Object>> orderItem (@SessionAttribute(name = SessionUtils.LOGIN_SESSION) SessionVO sessionVO, @RequestBody OrderItemRequest request) throws RuntimeException {
 
-        SessionVO sessionVO = (SessionVO) session.getAttribute(SessionUtils.LOGIN_SESSION);
         String userId = sessionVO.getId();
 
         orderService.order(userId, request.getItemId(), request.getCount());
@@ -41,6 +43,23 @@ public class OrderApiController {
         Map<String, Object> result = new HashMap<>();
         result.put("data", true);
         result.put("message", MessageConstants.ORDER_SUCCESS);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /**
+     * 주문 취소
+     * @param orderId
+     * @return
+     */
+    @DeleteMapping("/order/{orderId}")
+    public ResponseEntity<Map<String, Object>> cancelOrder (@PathVariable("orderId") Long orderId) {
+
+        orderService.cancelOrder(orderId);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("data", true);
+        result.put("message", MessageConstants.ORDER_CANCEL);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
