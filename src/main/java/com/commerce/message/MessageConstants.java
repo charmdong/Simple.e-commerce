17e03ddb76cb317b6d985a411d744995d2b6650e
Donggun.Chung <chungdk1117@gmail.com>
2022-09-18 21:45:15 +0900

    :hammer: 2022.09.18.Sun [상수 메시지 추가]

--- src/main/java/com/commerce/message/MessageConstants.java
+++ src/main/java/com/commerce/message/MessageConstants.java
@@ -3,6 +3,7 @@ package com.commerce.message;
 public class MessageConstants {
 
     public static final String ORDER_SUCCESS = "주문이 완료되었습니다";
+    public static final String ORDER_CANCEL = "주문이 취소되었습니다.";
 
     public static final String CART_DUP = "이미 장바구니에 존재합니다.";
     public static final String CART_ADD = "장바구니에 담았습니다.";
