5d91ea9b88b0389e154792fa51792cc420aceaae
Donggun.Chung <chungdk1117@gmail.com>
2022-09-21 22:08:52 +0900

    :hammer: 2022.09.21.Wed [review Repository 생성]

---
+++ src/main/java/com/commerce/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.commerce.repository;
+
+import com.commerce.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
